c513d5407ae19aea4bed72e9d7fdd2b0bcda97f4
WAY-C <57969839+WAY-C@users.noreply.github.com>
2021-08-05 15:58:28 +0800

    Update answer1.js

--- FED57 判断是否以元音字母结尾/answer1.js
+++ FED57 判断是否以元音字母结尾/answer1.js
@@ -1,17 +1,17 @@
 function endsWithVowel(str) {
-        switch(str[str.length-1]){
-            case 'a':
-            case 'e':
-            case 'i':
-            case 'o':
-            case 'u':
-            case 'A':
-            case 'E':
-            case 'I':
-            case 'O':
-            case 'U':
-                return true;
-            default:
-                return false;
-        }
+    switch(str[str.length-1]){
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'O':
+        case 'U':
+            return true;
+        default:
+            return false;
     }
+}
